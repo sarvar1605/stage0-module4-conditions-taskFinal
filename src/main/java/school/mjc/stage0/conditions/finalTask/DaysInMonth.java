3263f910f739dd55bf3c5974b2a60ffcc46c6ba9
Sarvarbek <112868538+sarvar1605@users.noreply.github.com>
2022-10-04 02:08:37 +0500

    Update DaysInMonth.java

--- src/main/java/school/mjc/stage0/conditions/finalTask/DaysInMonth.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/DaysInMonth.java
@@ -15,16 +15,22 @@ public class DaysInMonth {
                     case 4,6,9,11:
                         System.out.println("30");
                         break;
-                    default:
-                        System.out.println(""invalid date");
-                        break;
                      }
             } else {
-                System.out.println("year is not loop");
+                switch(month){
+                    case 1,3,5,7,8,10,12:
+                        System.out.println("31");
+                        break;
+                    case 2:
+                        System.out.println("28");
+                        break;
+                    case 4,6,9,11:
+                        System.out.println("30");
+                        break;
               } 
                      
           } else {
-           System.out.println(""invalid date");
+           System.out.println("invalid date");
            }
     }
 }
